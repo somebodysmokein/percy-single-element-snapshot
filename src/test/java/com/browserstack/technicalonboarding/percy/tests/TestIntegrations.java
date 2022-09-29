00054beaa330b65aa41f93a798f2a5b716b65c52
somebodysmokein <venky88r@gmail.com>
2022-09-29 18:21:30 +0530

    Updated with a new single element snapshot for Integrations page
    Integrations page update

--- src/test/java/com/browserstack/technicalonboarding/percy/tests/TestIntegrations.java
+++ src/test/java/com/browserstack/technicalonboarding/percy/tests/TestIntegrations.java
@@ -17,6 +17,9 @@ public class TestIntegrations extends TestRunner{
 		pg.goToUrl();
 		assertEquals(driver.getTitle(), pg.getIntegrationsTitle());
 		percy.snapshot("Integrations Page Test",Arrays.asList(new Integer[]{375, 480, 720, 1280, 1440, 1920}));
+
+		percy.snapshot("Single DOM Element-Integrations",Arrays.asList(new Integer[]{375, 480, 720, 1280, 1440, 1920}),
+				null,false,null,"#post-2245 > div:nth-child(1) > div > div > div > article > div > div > div.text-section.col-xs-12.no-pad.col-sm-6.col-md-8.col-lg-8 > div:nth-child(2) > div > div > a");
 		
 	}	
 }
